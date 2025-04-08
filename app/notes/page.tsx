c20849f77cf17061aa287b3f27d307c31bd32552
L-Ryland <saintleso@gmail.com>
2025-04-08 13:20:19 +0800

    style(notes): optimize embedded

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,12 +4,13 @@ export default function Notes() {
   return (
     <Container className="mt-9">
       <iframe
+        title="Ryland's Notion Knowledge Base"
         src="https://ryland-l.notion.site/ebd/1c437d2f92f480eda4aedc81451ba6c7?v=1c437d2f92f480f6a783000c04fcd7f6"
         width="100%"
         height="600"
-        frameBorder="0"
         loading="eager"
         allowFullScreen
+        className="border-none ring-none"
       />
     </Container>
   );
